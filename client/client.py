a83a3e2e4d86ef77d758bb7bcb8cf69642369ebc
Luiz Matos <luiz@debian>
2013-12-07 21:50:47 -0500

    Improving client

--- client/client.py
+++ client/client.py
@@ -1,14 +1,15 @@
-# telnet program example
 import socket, select, string, sys
- 
+
+def information(data) :
+    data.split(';')
+
 def prompt() :
     sys.stdout.flush()
  
-#main function
 if __name__ == "__main__":
      
     if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
+        print 'Usage : python client.py hostname port'
         sys.exit()
      
     host = sys.argv[1]
@@ -17,35 +18,30 @@ if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
      
-    # connect to remote host
     try :
         s.connect((host, port))
     except :
-        print 'Unable to connect'
+        print 'Error in connection'
         sys.exit()
      
-    print 'Connected to remote host. Start sending messages'
+    print 'Connected to VTNT. Start sending commands'
     prompt()
      
     while 1:
         socket_list = [sys.stdin, s]
          
-        # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
          
         for sock in read_sockets:
-            #incoming message from remote server
             if sock == s:
                 data = sock.recv(4096)
                 if not data :
                     print '\nDisconnected from chat server'
                     sys.exit()
                 else :
-                    #print data
                     sys.stdout.write(data)
                     prompt()
              
-            #user entered a message
             else :
                 msg = sys.stdin.readline()
                 s.send(msg)
